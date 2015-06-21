7a07511e2eb74c137d76749edcc5cdb7f94f74f3
Eduardo Flores <eduardo@enelmargen.org>
2015-06-21 12:24:01 -0500

    install errors

--- R/Catalogo_Test.R
+++ R/Catalogo_Test.R
@@ -1,2 +1,2 @@
 #catalogo
-pib2008_total<-http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/381016/00000/es/false/xml/
+#pib2008_total<-http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/#381016/00000/es/false/xml/
