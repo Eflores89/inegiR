26be15d4c65f5f82b7373ccfb7778400c08057a6
Eduardo Flores <eduardo@enelmargen.org>
2015-07-08 23:12:53 -0500

    add ordenar_porconteo, mejoras a denue_varios_stats, cambios esteticos descripciones

--- R/FuncionesAuxiliares.R
+++ R/FuncionesAuxiliares.R
@@ -14,7 +14,7 @@
 #'
 #' @examples
 #' #Calcular la inflación (Ver Inflacion_Inegi() para un método más directo)
-#' INPC<-Serie_Inegi(INPC,token)
+#' INPC<-serie_inegi(INPC,token)
 #' Inflacion<-YoY(INPC$Valores,12)
 #' @export
 #' 
@@ -33,4 +33,36 @@ YoY<-function (serie,lapso,decimal=TRUE){
       if(decimal) {return(s)}
           else    {return(s*100)}
   }
+}
+#' Ordenar por conteo de factores
+#'
+#' Wrapper para ordenar rapidamente de mayor a menor por grupos un data.frame. 
+#'
+#' @param df Data.frame a condensar
+#' @param col Columna con factores. Se pone sin parentesis.
+#'
+#' @author Eduardo Flores 
+#' @return Data.frame
+#' @seealso denue_varios_stats 
+#' @examples
+#' #MWE
+#' df<-data.frame(factores=c("A","A","B","C","C","D","A","A"),otros=c(1,3,2,4,5,1,2,7))
+#' 
+#' #Ordenar, de mayor a menor, por conteo de factores
+#' PorConteo<-ordenar_porconteo(df,factores)
+#' 
+#' @export
+#' 
+
+ordenar_porconteo<-function(df,col)
+{ #para poner solamente el nombre de columna
+  columna<-as.character(eval(substitute(col),df, parent.frame()))
+  
+  # agrupar
+  set<-aggregate(x = df,by = list(columna),FUN = length)
+  set<-set[,names(set)[1:2]]
+  # ordenar mayor a menor
+  ordenado<-set[order(set[,names(set[2])],decreasing=TRUE),]
+  # export
+  return(ordenado)
 }
