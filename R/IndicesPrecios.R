c39da6b7d0d837fbaea29d5fe28c4b16aa3889a2
Eduardo Flores <eduardo@enelmargen.org>
2015-09-18 14:13:32 -0500

    added inflacion ciudades

--- R/IndicesPrecios.R
+++ R/IndicesPrecios.R
@@ -97,3 +97,110 @@ inflacion_tot<-function(token)
   d<-cbind.data.frame(Fechas=df$Fechas,Valores=df$ToT)
   return(d)
 }
+
+#' Obtener inflacion por Ciudad
+#'
+#' Obtiene la tasa de inflación mensual por ciudad. 
+#' Es un wrapper de las funciones \code{serie_inegi()} y \code{YoY()}. 
+#'
+#' @param token token personal emitido por el INEGI para acceder al API.
+#' @author Eduardo Flores 
+#' @return Data.frame 
+#'
+#' @examples
+#' InflacionCiudades<-inflacion_ciudades(token)
+#' @note Encoding no permite acentos en titulo de descripción
+#' @export
+#'
+
+inflacion_ciudades<-function(token){
+  #Series de INPC;
+  SeriesDf<-
+    data.frame(
+  "Ciudad"=c(
+      "DF","Merida","Morelia","Guadalajara","Monterrey",
+      "Mexicali","CdJuarez","Acapulco","Culiacan","Leon",
+      "Puebla","SanLuisPotosi","Tapachula","Toluca","Torreon",
+      "Veracruz","Villahermosa","Tampico","Chihuahua","Hermosillo","Monclova",
+      "Cordoba","Ags","Tijuana","Matamoros","Colima","LaPaz","Chetumal",
+      "Jacona","Fresnillo","Iguala","Huatabampo","Tulancingo","Cortazar",
+      "CdJimenez","Durango","Tepic","Oaxaca","Queretaro","Cuernavaca",
+      "Tlaxcala","SanAndres","Campeche","Tepatitlan","Tehuantepec","CdAcuna"), 
+  "Data"=c(
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216095/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216096/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216097/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216098/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216099/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216100/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216101/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216102/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216103/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216104/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216105/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216106/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216107/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216108/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216109/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216110/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216111/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216112/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216113/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216114/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216115/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216116/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216117/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216118/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216119/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216120/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216121/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216122/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216123/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216124/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216125/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216126/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216127/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216128/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216129/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216130/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216131/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216132/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216133/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216134/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216135/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216136/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216137/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216138/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216139/00000/en/false/xml/",
+  "http://www3.inegi.org.mx/sistemas/api/indicadores/v1//Indicador/216140/00000/en/false/xml/"
+    ), 
+  stringsAsFactors = FALSE)
+  
+  # download
+  dloads<-list()
+  for(i in 1:46)
+  {
+    s<-SeriesDf$Data[i]
+    dloads[[i]]<-inegiR::serie_inegi(serie = s, 
+                                     token)
+  }
+  
+  # names
+  names(dloads)<-as.character(SeriesDf$Ciudad)
+  for(i in 1:46)
+  {
+    names(dloads[[i]])<-c(names(dloads[i]),"Fechas")
+  }
+  
+  #join
+  df<-Reduce(function(...) merge(...,all=T),dloads)
+  
+  # year over year
+  ts<-apply(df[,2:47],
+            2, function(x){
+              inegiR::YoY(serie = x, lapso = 12, decimal = FALSE)})
+  ts<-as.data.frame(ts)
+  # bind
+  ts$Fechas<-df$Fechas
+  return(ts)
+}
